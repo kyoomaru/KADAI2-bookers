62ea8ece42a94ffacdf7f62c57c22c37c69de54c
kyoomaru <138374899+kyoomaru@users.noreply.github.com>
2023-07-29 02:15:15 +0000

    [indexまで作成]bookers

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'books/new'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show',  as:'book'
+  get 'books/:id' => 'books#edit'
+  get '/' => 'homes#top'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
