f0c33c6aced0dbd68ee30a2c377b9e763a80c91e
kyoomaru <138374899+kyoomaru@users.noreply.github.com>
2023-07-30 02:11:56 +0000

    [updateまで済]bookers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show',  as:'book'
+  get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get '/' => 'homes#top'
   patch 'books/:id' => 'books#update', as: 'update_book'
